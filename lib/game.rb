f6c427abf43547bf8ec18c7015f71062b0efef40
Robert <fishwickrob@live.com>
2018-05-03 15:08:39 +0100

    Switching turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,20 @@
 class Game
-attr_reader :player_1, :player_2
+attr_reader :player_1, :player_2, :counter
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    @counter = 1
   end
 
-  def attack(player)
-    player.receive_damage
+  def attack
+    if @counter.odd?
+     @player_2.receive_damage
+     @counter += 1
+   else
+     @player_1.receive_damage
+     @counter += 1
+   end
   end
+
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,8 @@
 require 'game'
 
 describe Game do
- let(:p1) {double :player}
- let(:p2) {double :player}
+ let(:p1) {double :player, receive_damage: true}
+ let(:p2) {double :player, receive_damage: true}
  subject(:game) {Game.new(p1, p2)}
 
  it 'responds to method player_1' do
@@ -14,12 +14,35 @@ describe Game do
  end
   describe '#attack' do
     it 'responds to attack method' do
-      expect(game).to respond_to(:attack).with(1).argument
+      expect(game).to respond_to(:attack)
     end
 
-    it 'should cause argument to recieve method recieve_damage' do
+    it 'expects player_2 to be attacked' do
+      expect(p2).to receive(:receive_damage)
+      game.attack
+    end
+
+    it 'expects player_q to be attacked' do
+      game.attack
       expect(p1).to receive(:receive_damage)
-      game.attack(p1)
+      game.attack
+    end
+  end
+
+  describe 'counter' do
+    it 'should default to 1' do
+      expect(game.counter).to eq(1)
+    end
+
+    it 'when attack counter should equal 2' do
+      game.attack
+      expect(game.counter).to eq(2)
+    end
+
+    it 'when attacked twice counter should equal 3' do
+      game.attack
+      game.attack
+      expect(game.counter).to eq(3)
     end
   end
 
